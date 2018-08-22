672ebb3a96d22852b85503b53111ef51a3f3e459
Oscar Djupfeldt <oscar.z.djupfeldt@teliacompany.com>
2018-08-22 15:44:25 +0200

    Check skip before create baseline is executed

--- src/main/java/com/github/ozsie/CreateBaselineMojo.kt
+++ src/main/java/com/github/ozsie/CreateBaselineMojo.kt
@@ -4,12 +4,12 @@ import io.gitlab.arturbosch.detekt.cli.parseArguments
 import io.gitlab.arturbosch.detekt.cli.runners.Runner
 import org.apache.maven.plugins.annotations.Mojo
 
-
-
 @Suppress("unused")
 @Mojo(name = "create-baseline")
 open class CreateBaselineMojo : DetektMojo() {
-    override fun execute() = Runner(parseArguments(cliString)).execute()
+    override fun execute() {
+        if (!skip) Runner(parseArguments(cliString)).execute()
+    }
     private val cliString get() = getCliSting().apply { add(CREATE_BASELINE) }.log().toTypedArray()
 }
 
