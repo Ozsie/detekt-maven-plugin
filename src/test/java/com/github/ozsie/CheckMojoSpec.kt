222333c067a05c70bd039bf018eb10c31956edc2
Ozsie <oscar@djupfeldt.se>
2018-08-23 21:46:14 +0200

    Added test cases for all mojos

---
+++ src/test/java/com/github/ozsie/CheckMojoSpec.kt
@@ -0,0 +1,34 @@
+package com.github.ozsie
+
+import com.beust.jcommander.ParameterException
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertFailsWith
+import kotlin.test.expect
+
+class CheckMojoSpec : Spek({
+    given("a CheckMojo and 'skip' is true") {
+        val checkMojo = CheckMojo()
+        checkMojo.skip = true
+        on("checkMojo.execute()") {
+            test("unit is expected") {
+                expect(Unit) {
+                    checkMojo.execute()
+                }
+            }
+        }
+    }
+
+    given("a CheckMojo and 'skip' is false") {
+        val checkMojo = CheckMojo()
+        checkMojo.skip = false
+        on("checkMojo.execute()") {
+            test("ParameterException is thrown") {
+                assertFailsWith(ParameterException::class) {
+                    checkMojo.execute()
+                }
+            }
+        }
+    }
+})
---
+++ src/test/java/com/github/ozsie/GenerateConfigMojoSpec.kt
@@ -0,0 +1,61 @@
+package com.github.ozsie
+
+import com.beust.jcommander.ParameterException
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.on
+import java.io.File
+import kotlin.test.assertTrue
+import kotlin.test.expect
+
+class GenerateConfigMojoSpec : Spek({
+    given("a GenerateConfigMojo and 'skip' is true") {
+        val generateConfigMojo = GenerateConfigMojo()
+        generateConfigMojo.skip = true
+        on("generateConfigMojo.execute()") {
+            test("unit is expected") {
+                expect(Unit) {
+                    generateConfigMojo.execute()
+                }
+            }
+        }
+    }
+
+    given("a GenerateConfigMojo and 'skip' is false") {
+        val generateConfigMojo = GenerateConfigMojo()
+        generateConfigMojo.skip = false
+        on("generateConfigMojo.execute()") {
+            test("Config file is generated") {
+                generateConfigMojo.execute()
+                val file = File("default-detekt-config.yml")
+                assertTrue(file.exists())
+                file.deleteOnExit()
+            }
+        }
+    }
+
+    given("a GCMojo and 'skip' is true") {
+        val generateConfigMojo = GCMojo()
+        generateConfigMojo.skip = true
+        on("generateConfigMojo.execute()") {
+            test("unit is expected") {
+                expect(Unit) {
+                    generateConfigMojo.execute()
+                }
+            }
+        }
+    }
+
+    given("a GCMojo and 'skip' is false") {
+        val generateConfigMojo = GCMojo()
+        generateConfigMojo.skip = false
+        on("generateConfigMojo.execute()") {
+            test("Config file is generated") {
+                generateConfigMojo.execute()
+                val file = File("default-detekt-config.yml")
+                assertTrue(file.exists())
+                file.deleteOnExit()
+            }
+        }
+    }
+})
