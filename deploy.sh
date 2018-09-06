95fb67db0e2f771b59529f9329d9452449b3a509
Ozsie <oscar@djupfeldt.se>
2018-09-06 21:14:19 +0200

    Fixed travis yaml

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl -fL https://getcli.jfrog.io | sh
-chmod +x jfrog
-./jfrog bt vc --user $BIN_TRAY_USER --key $BIN_TRAY_PWD 1.0.0.RC8-1-SNAPSHOT
-./jfrog bt u --user $BIN_TRAY_USER --key $BIN_TRAY_PWD --override --publish "target/detekt-maven-plugin*" ozsie/maven/detekt-maven-plugin/1.0.0.RC8-1-SNAPSHOT
