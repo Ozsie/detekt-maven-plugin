4470f5ede6ad60ad0c9d7a0477e0e85ec596543b
Sebastian Sigl <sebastian.sigl@adevinta.com>
2022-07-23 13:54:53 +0200

    Add default classpath
    Closes #144

--- src/main/java/com/github/ozsie/CheckMojo.kt
+++ src/main/java/com/github/ozsie/CheckMojo.kt
@@ -1,6 +1,7 @@
 package com.github.ozsie
 
 import io.github.detekt.tooling.api.MaxIssuesReached
+import io.gitlab.arturbosch.detekt.cli.CliArgs
 import io.gitlab.arturbosch.detekt.cli.parseArguments
 import io.gitlab.arturbosch.detekt.cli.runners.Runner
 import org.apache.maven.plugins.annotations.LifecyclePhase
@@ -15,16 +16,27 @@ import java.nio.file.Paths
         threadSafe = true,
         requiresDependencyCollection = ResolutionScope.TEST)
 class CheckMojo : DetektMojo() {
+    lateinit var cliArgs: CliArgs
+
     override fun execute() {
         getCliSting().forEach {
             log.debug("Applying $it")
         }
-        val cliArgs = parseArguments(getCliSting().log().toTypedArray())
+        this.cliArgs = parseArguments(getCliSting().log().toTypedArray())
+
         val foundInputDir = Files.isDirectory(Paths.get(input))
-        if (!skip && foundInputDir)
+        if (!skip && foundInputDir) {
+            setDefaultClasspathIfNotSet(cliArgs)
             failBuildIfNeeded { Runner(cliArgs, System.out, System.err).execute() }
-        else
+        } else
             inputSkipLog(skip)
+    }
+
+    private fun setDefaultClasspathIfNotSet(cliArgs: CliArgs) {
+        if (cliArgs.classpath.isNullOrBlank()) {
+            cliArgs.classpath = mavenProject?.compileClasspathElements?.joinToString(
+                java.io.File.pathSeparatorChar.toString())
+        }
     }
 
     private fun failBuildIfNeeded(block: () -> Unit) {
