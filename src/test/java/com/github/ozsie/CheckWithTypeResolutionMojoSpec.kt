e03de31f1eb1f36fe9f9646f5adc38f6f14b8fe4
Ozsie <oscar@djupfeldt.se>
2022-11-17 14:28:21 +0100

    Move check with type resolution as own goal

--- src/test/java/com/github/ozsie/CheckWithTypeResolutionMojoSpec.kt
+++ src/test/java/com/github/ozsie/CheckWithTypeResolutionMojoSpec.kt
@@ -1,5 +1,6 @@
 package com.github.ozsie
 
+import com.github.ozsie.test.CheckWithTypeResolutionMojoTestFactory
 import io.github.detekt.tooling.api.MaxIssuesReached
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
@@ -7,14 +8,9 @@ import org.jetbrains.spek.api.dsl.on
 import kotlin.test.assertFailsWith
 import kotlin.test.expect
 
-class CheckMojoSpec : Spek({
-    val invalidPackageNamingDirectoryPath by lazy {
-        CheckMojoSpec::class.java.classLoader.getResource("code-samples/invalid-package-naming")!!
-            .file
-    }
-
+class CheckWithTypeResolutionMojoSpec : Spek({
     given("a CheckMojo and 'skip' is true") {
-        val checkMojo = CheckMojo()
+        val checkMojo = CheckWithTypeResolutionMojo()
         checkMojo.skip = true
         on("checkMojo.execute()") {
             test("unit is expected") {
@@ -26,7 +22,7 @@ class CheckMojoSpec : Spek({
     }
 
     given("a CheckMojo and 'skip' is false") {
-        val checkMojo = CheckMojo()
+        val checkMojo = CheckWithTypeResolutionMojo()
         checkMojo.skip = false
         on("checkMojo.execute()") {
             test("Unit is expected") {
@@ -38,11 +34,11 @@ class CheckMojoSpec : Spek({
     }
 
     given("a CheckMojo and 'failBuildOnMaxIssuesReached' is false") {
-        val checkMojo = CheckMojo().apply {
-            input = invalidPackageNamingDirectoryPath
+        val checkMojo = CheckWithTypeResolutionMojoTestFactory.createWithInvalidPackageNamingStructure {
             failBuildOnMaxIssuesReached = false
-            failFast = true // fail on any issue
+            failFast = true
         }
+
         on("checkMojo.execute()") {
             test("Unit is expected") {
                 expect(Unit) {
@@ -53,15 +49,42 @@ class CheckMojoSpec : Spek({
     }
 
     given("a CheckMojo and 'failBuildOnMaxIssuesReached' is true") {
-        val checkMojo = CheckMojo().apply {
-            input = invalidPackageNamingDirectoryPath
+        val checkMojo = CheckWithTypeResolutionMojoTestFactory.createWithInvalidPackageNamingStructure {
             failBuildOnMaxIssuesReached = true
-            failFast = true // fail on any issue
+            failFast = true
         }
         on("checkMojo.execute()") {
             test("Unit is expected") {
                 assertFailsWith(MaxIssuesReached::class, "Build failed with 1 weighted issues.") {
                     checkMojo.execute()
+                }
+            }
+        }
+    }
+
+    given("classpath parameter") {
+        val checkMojo = CheckWithTypeResolutionMojoTestFactory.createWithNoRuleExecution {
+            classPath = "/tmp/provided"
+        }
+
+        on("checkMojo.execute()") {
+            test("uses provider value") {
+                expect("/tmp/provided") {
+                    checkMojo.execute()
+                    checkMojo.cliArgs.classpath
+                }
+            }
+        }
+    }
+
+    given("no classpath parameter") {
+        val checkMojo = CheckWithTypeResolutionMojoTestFactory.createWithNoRuleExecution()
+
+        on("checkMojo.execute()") {
+            test("uses default compileClasspathElements") {
+                expect("/tmp/default${java.io.File.pathSeparatorChar}/tmp/default2") {
+                    checkMojo.execute()
+                    checkMojo.cliArgs.classpath
                 }
             }
         }
